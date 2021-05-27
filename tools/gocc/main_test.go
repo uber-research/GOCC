036c9de6bb256a8bf1d4173c416e527e846f4ab9
Milind Chabbi <milind@uber.com>
2021-05-27 12:27:42 -0700

    Mov Zap to go.uber.org.

--- tools/gocc/main_test.go
+++ tools/gocc/main_test.go
@@ -20,42 +20,53 @@ import (
 func TestPkgs(t *testing.T) {
 
 	tt := []struct {
-		loc      string
-		rev      string
-		name     string
-		diffFile string
+		loc         string
+		rev         string
+		name        string
+		diffFile    string
+		rewriteTest bool
 	}{
-		// {
-		// 	loc:      "./testdata/src/github.com/uber-go/tally",
-		// 	name:     "tally",
-		// 	rev:      "164eb6a3c0d4c8c82e5a63a8a12d506f0c9b2637",
-		// 	diffFile: "testdata/tally.diff",
-		// },
-
 		{
-			loc:      "./testdata/src/github.com/uber-go/zap",
-			name:     "zap",
-			rev:      "5b4722d3797ca2e22f3c16dbe71a97b9c7783c98",
-			diffFile: "testdata/zap.diff",
+			loc:         "./testdata/src/github.com/uber-go/tally",
+			name:        "tally",
+			rev:         "164eb6a3c0d4c8c82e5a63a8a12d506f0c9b2637",
+			diffFile:    "testdata/tally.diff",
+			rewriteTest: false,
 		},
 
-		// {
-		// 	loc:      "./testdata/src/github.com/VictoriaMetrics/fastcache",
-		// 	name:     "fastcache",
-		// 	rev:      "5b4722d3797ca2e22f3c16dbe71a97b9c7783c98",
-		// 	diffFile: "testdata/fastcache.diff",
-		// },
+		// // {
+		// // 	loc:      "./testdata/src/go.uber.org/zap",
+		// // 	name:     "zap",
+		// // 	rev:      "5b4722d3797ca2e22f3c16dbe71a97b9c7783c98",
+		// // 	diffFile: "testdata/zap.diff",
+		// // },
+
+		{
+			loc:         "./testdata/src/github.com/patrickmn/go-cache/",
+			name:        "go-cache",
+			rev:         "5b4722d3797ca2e22f3c16dbe71a97b9c7783c98",
+			diffFile:    "testdata/gocache.diff",
+			rewriteTest: true,
+		},
+
+		{
+			loc:         "./testdata/src/github.com/VictoriaMetrics/fastcache",
+			name:        "fastcache",
+			rev:         "5b4722d3797ca2e22f3c16dbe71a97b9c7783c98",
+			diffFile:    "testdata/fastcache.diff",
+			rewriteTest: false,
+		},
 	}
 
 	var replaceRegex = regexp.MustCompile(`(?s)\nindex(.*?)\n`)
 
 	for _, tc := range tt {
 		_ = tc
-		// defer func() {
-		// 	cmd := []string{"git", "checkout", "--", tc.loc}
-		// 	exec.Command(cmd[0], cmd[1:]...).Output()
-		// }()
-		g := NewGOCC(false, false, false, true, true, false, tc.loc)
+		defer func() {
+			cmd := []string{"git", "checkout", "--", tc.loc}
+			exec.Command(cmd[0], cmd[1:]...).Output()
+		}()
+		g := NewGOCC(false, tc.rewriteTest, false, true, true, false, tc.loc)
 		g.Process()
 
 		// Get diff
